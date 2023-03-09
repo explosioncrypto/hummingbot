6ce164765eb449969ea74961d63767a17d93b686
Tomas <explosioncrypto@gmail.com>
2023-03-09 08:01:21 +0100

    Update vitex_order_book.pyx

--- hummingbot/connector/exchange/vitex/vitex_order_book.pyx
+++ hummingbot/connector/exchange/vitex/vitex_order_book.pyx
@@ -28,29 +28,47 @@ cdef class VitexOrderBook(OrderBook):
     def snapshot_message_from_exchange(cls,
                                        msg: Dict[str, any],
                                        timestamp: float,
-                                       metadata: Optional[Dict]=None) -> OrderBookMessage:
+                                       metadata: Optional[Dict] = None) -> OrderBookMessage:
         if metadata:
             msg.update(metadata)
-        return OrderBookMessage(OrderBookMessageType.SNAPSHOT, msg, timestamp)
+        return OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
+            "trading_pair": VitexAPI.convert_from_exchange_trading_pair(msg["trading_pair"]),
+            "update_id": msg["timestamp"],
+            "bids": msg["bids"],
+            "asks": msg["asks"]
+        }, timestamp=timestamp)
 
     @classmethod
     def diff_message_from_exchange(cls,
                                    msg: Dict[str, any],
-                                   timestamp: Optional[float]=None,
-                                   metadata: Optional[Dict]=None) -> OrderBookMessage:
+                                   timestamp: Optional[float] = None,
+                                   metadata: Optional[Dict] = None) -> OrderBookMessage:
         if metadata:
             msg.update(metadata)
-        return OrderBookMessage(OrderBookMessageType.DIFF, msg, timestamp)
+        # TODO: ViteX Websocket API does not support incremental depth messages, use snapshot message instead
+        message_type = OrderBookMessageType.DIFF
+
+        return OrderBookMessage(message_type, {
+            "trading_pair": VitexAPI.convert_from_exchange_trading_pair(msg["topic"].split(".")[1]),
+            "update_id": msg["timestamp"],
+            "bids": msg["data"]["bids"],
+            "asks": msg["data"]["asks"]
+        }, timestamp=timestamp)
 
     @classmethod
-    def trade_message_from_exchange(cls,
-                                    msg: Dict[str, any],
-                                    metadata: Optional[Dict]=None):
+    def trade_message_from_exchange(cls, msg: Dict[str, any], metadata: Optional[Dict] = None):
         if metadata:
             msg.update(metadata)
-        ts = metadata["ts"]
-        data = msg["data"][0]
-        return OrderBookMessage(OrderBookMessageType.TRADE, msg, ts)
+        ts = msg["timestamp"]
+        data = msg["data"][1]
+        return OrderBookMessage(OrderBookMessageType.TRADE, {
+            "trading_pair": VitexAPI.convert_from_exchange_trading_pair(data["s"]),
+            "trade_type": VitexAPI.convert_trade_type(data["side"]),
+            "trade_id": data["id"],
+            "update_id": ts,
+            "price": data["p"],
+            "amount": data["q"]
+        }, timestamp=ts * 1e-3)
 
     @classmethod
     def from_snapshot(cls, msg: OrderBookMessage) -> "OrderBook":
