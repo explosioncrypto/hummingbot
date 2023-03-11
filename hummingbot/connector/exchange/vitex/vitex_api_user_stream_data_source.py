53acd2ceba7dcc4561efe58bd6c79525c75ccae1
Tomas <explosioncrypto@gmail.com>
2023-03-11 23:20:32 +0100

    Update vitex_api_user_stream_data_source.py

--- hummingbot/connector/exchange/vitex/vitex_api_user_stream_data_source.py
+++ hummingbot/connector/exchange/vitex/vitex_api_user_stream_data_source.py
@@ -3,14 +3,10 @@
 import asyncio
 import logging
 import time
-from typing import (
-    Any,
-    AsyncIterable,
-    Dict,
-    Optional
-)
+from typing import Any, AsyncIterable, Dict, Optional
 import ujson
 import websockets
+from async_timeout import timeout
 from hummingbot.core.data_type.user_stream_tracker_data_source import UserStreamTrackerDataSource
 from hummingbot.logger import HummingbotLogger
 
@@ -18,20 +14,20 @@ STREAM_URL = "wss://api.vitex.net/v2/ws"
 
 
 class VitexAPIUserStreamDataSource(UserStreamTrackerDataSource):
-    MESSAGE_TIMEOUT = 30.0
-    PING_TIMEOUT = 10.0
+    MESSAGE_TIMEOUT = 10.0
+    # PING_TIMEOUT = 10.0  # Not used
 
-    _bausds_logger: Optional[HummingbotLogger] = None
+    _vitex_logger: Optional[HummingbotLogger] = None
 
     @classmethod
     def logger(cls) -> HummingbotLogger:
-        if cls._bausds_logger is None:
-            cls._bausds_logger = logging.getLogger(__name__)
-        return cls._bausds_logger
+        if cls._vitex_logger is None:
+            cls._vitex_logger = logging.getLogger(__name__)
+        return cls._vitex_logger
 
     def __init__(
-            self,
-            vite_address: Optional[str] = None
+        self,
+        vite_address: Optional[str] = None
     ):
         self._vite_address: str = vite_address
         self._last_recv_time: float = 0
@@ -42,49 +38,23 @@ class VitexAPIUserStreamDataSource(UserStreamTrackerDataSource):
         return self._last_recv_time
 
     async def _inner_messages(self, ws: websockets.WebSocketClientProtocol) -> AsyncIterable[Any]:
-        # Terminate the recv() loop as soon as the next message timed out, so the outer loop can reconnect.
         try:
             while True:
-                try:
-                    msg: str = await asyncio.wait_for(ws.recv(), timeout=self.MESSAGE_TIMEOUT)
-                    msg_json = ujson.loads(msg)
-                    event = msg_json["event"]
-                    if event == "push":
-                        self._last_recv_time = time.time()
-                        yield msg_json
-                except asyncio.TimeoutError:
-                    try:
-                        await ws.send('{"command":"ping"}')
-                        self._last_recv_time = time.time()
-                    except asyncio.TimeoutError:
-                        raise
+                with timeout(self.MESSAGE_TIMEOUT):
+                    msg: str = await ws.recv()
+                msg_json = ujson.loads(msg)
+                event = msg_json["event"]
+                if event == "push":
+                    self._last_recv_time = time.time()
+                    yield msg_json
         except asyncio.TimeoutError:
-            self.logger().warning("WebSocket ping timed out. Going to reconnect...")
-            return
+            self.logger().warning("WebSocket message timed out.")
         except websockets.exceptions.ConnectionClosed:
-            return
-        finally:
-            await ws.close()
+            self.logger().warning("WebSocket connection closed unexpectedly.")
 
     async def listen_for_user_stream(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
         while True:
             try:
                 topic = f"order.{self._vite_address}"
                 async with websockets.connect(STREAM_URL) as ws:
-                    ws: websockets.WebSocketClientProtocol = ws
-                    subscribe_request: Dict[str, Any] = {
-                        "command": "sub",
-                        "params": [topic]
-                    }
-                    # Subscribe topics
-                    await ws.send(ujson.dumps(subscribe_request))
-                    # Receive and parse messages
-                    async for msg in self._inner_messages(ws):
-                        user_message = msg["data"]
-                        output.put_nowait(user_message)
-            except asyncio.CancelledError:
-                raise
-            except Exception:
-                self.logger().error("Unexpected error with WebSocket connection. Retrying after 30 seconds...",
-                                    exc_info=True)
-                await asyncio.sleep(30.0)
+                    self._last_recv_time
