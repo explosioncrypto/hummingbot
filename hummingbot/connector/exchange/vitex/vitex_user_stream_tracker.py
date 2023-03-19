65956c3c5192fd75f234360cc1b5e0d2ed4c76fa
Tomas <explosioncrypto@gmail.com>
2023-03-19 19:38:58 +0100

    Update vitex_user_stream_tracker.py

--- hummingbot/connector/exchange/vitex/vitex_user_stream_tracker.py
+++ hummingbot/connector/exchange/vitex/vitex_user_stream_tracker.py
@@ -30,7 +30,7 @@ class VitexUserStreamTracker(UserStreamTracker):
         vite_address: Optional[str] = None,
         trading_pairs: Optional[List[str]] = [],
     ):
-        super().__init__()
+        super().__init__(data_source=VitexAPIUserStreamDataSource(
         self._vite_address: str = vite_address
         self._trading_pairs: List[str] = trading_pairs
         self._ev_loop: asyncio.events.AbstractEventLoop = asyncio.get_event_loop()
